7cd57677dd03779ec9e10f4f619c329c4e27926a
wintwh521 <samuelchee521@gmail.com>
2025-10-03 14:23:50 +0800

    Update bot.py
    -modify run_web() to listening port from render

--- bot.py
+++ bot.py
@@ -704,7 +704,9 @@ def home():
 
 
 def run_web():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
